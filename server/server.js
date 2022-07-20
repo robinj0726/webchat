1a1f49ad547830768f570d865ae853ab781f1db3
robinj730 <88188467+robinj730@users.noreply.github.com>
2022-07-20 22:03:13 +0800

    Update server.js
    setup socket.io server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,15 @@ const app = new Koa();
 const port = 4000;
 
 const server = require('http').createServer(app.callback());
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('a user connected');
+    });
+});
+
 server.listen(port, () => {
-    console.log(`the server listening on port ${port}`)
+    console.log(`Server listening on port ${port}`)
 });
