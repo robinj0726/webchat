0a621a05da990e2ffa0673b94c6fb8d8451e8879
robinj730 <88188467+robinj730@users.noreply.github.com>
2022-07-20 23:43:13 +0800

    serve static files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const Koa = require('koa');
 const app = new Koa();
+const serve = require('koa-static');
 const port = process.env.PORT || 4000;
+
+app.use(serve(__dirname + '/../client/dist/'));
 
 const server = require('http').createServer(app.callback());
 const io = require('socket.io')(server);
