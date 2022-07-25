ee7a9473d582d9d538cfc128e7a8f5f14cfb28b4
robinj730 <88188467+robinj730@users.noreply.github.com>
2022-07-25 21:57:58 +0800

    ui for home page

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,45 +1,35 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import Textfield from '@smui/textfield';
+  import Button from '@smui/button';
+
+  let roomNo = '';
+  let userName = '';
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <Textfield 
+      style="width: 30%;"
+      bind:value={roomNo} 
+      label="Room Number:">
+    </Textfield>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
+  <div>
+    <Textfield 
+      style="width: 30%;"
+      bind:value={userName} 
+      label="Nick Name:">
+    </Textfield>
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <div style="margin: 1rem;">
+    <Button variant="unelevated">Join Meeting</Button>
+  </div>  
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  main {
+    margin-top: 15rem;
+    padding: 0;
+    text-align: center;
   }
 </style>
