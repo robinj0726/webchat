52fdd5a234afd07168e6afbf27dca30cd6097408
robinj730 <88188467+robinj730@users.noreply.github.com>
2022-07-29 21:38:27 +0800

    set proxy for debug

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/': {
+        target: 'https://39.106.13.131:4000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
   plugins: [svelte()]
 })
